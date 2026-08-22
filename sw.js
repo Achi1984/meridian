3a8f787b22e95130a11646427e1ccfd66d89769a
Achi1984 <v9jmbgw4nm@privaterelay.appleid.com>
2026-08-23 00:00:24 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+// ACHI MERIDIAN v5.3.1 — cache killer
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
-  .then(()=>self.registration.unregister()).then(()=>self.clients.claim())
+  .then(()=>self.registration.unregister())
+  .then(()=>self.clients.claim())
 ));
 self.addEventListener('fetch',()=>{});
