73b574eb71368f1ab70e7e26f9a1207dd34ebed1
Achi1984 <v9jmbgw4nm@privaterelay.appleid.com>
2026-08-26 16:54:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// MERIDIAN v5.80 cache reset
+// MERIDIAN v5.81 cache reset
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil((async()=>{
 for(const k of await caches.keys())await caches.delete(k);
