add351dd50f538604c062865e8ada6585dc4f30b
Achi1984 <v9jmbgw4nm@privaterelay.appleid.com>
2026-09-04 20:47:25 +0200

    MERIDIAN v7.62 R3: bump compatibility loader cache

--- app-v6.06.js
+++ app-v6.06.js
@@ -1,7 +1,7 @@
 /* MERIDIAN compatibility loader. The legacy filename remains referenced by index.html. */
 (function(){
   'use strict';
-  const TAG='7.62-R2';
+  const TAG='7.62-R3';
   const src=x=>x+'?v='+TAG;
   if(document.readyState==='loading'){
     document.write('<script src="'+src('app-v7.32-legacy.js')+'"><\/script>');
